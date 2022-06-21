75a8af7c027de1f853864824c56b978b4ca84f2d
ps217531 <ps217531@edu.summacollege.nl>
2022-06-21 20:17:19 +0200

    seeder done

--- Nick/project5/project5/database/seeders/OefeningenSeeder.php
+++ Nick/project5/project5/database/seeders/OefeningenSeeder.php
@@ -17,9 +17,51 @@ class OefeningenSeeder extends Seeder
     public function run()
     {
         DB::table('oefeningen')->insert([
-            'oefeningen' => 'Push-up ',
+            'oefeningen' => 'Push-up',
             'Beschrijving' => 'pushup.txt',
             'foto' => 'pushup.jpg',
+
+        ]);
+
+        DB::table('oefeningen')->insert([
+            'oefeningen' => 'Squat',
+            'Beschrijving' => 'squat.txt',
+            'foto' => 'squat.jpg',
+
+        ]);
+
+        DB::table('oefeningen')->insert([
+            'oefeningen' => 'Dip',
+            'Beschrijving' => 'dip.txt',
+            'foto' => 'dip.jpg',
+
+        ]);
+
+        DB::table('oefeningen')->insert([
+            'oefeningen' => 'Plank',
+            'Beschrijving' => 'plank.txt',
+            'foto' => 'plank.jpg',
+
+        ]);
+
+        DB::table('oefeningen')->insert([
+            'oefeningen' => 'Paardentrap',
+            'Beschrijving' => 'paardentrap.txt',
+            'foto' => 'paardentrap.jpg',
+
+        ]);
+
+        DB::table('oefeningen')->insert([
+            'oefeningen' => 'Mountain climber',
+            'Beschrijving' => 'mountainclimber.txt',
+            'foto' => 'mountainclimber.jpg',
+
+        ]);
+
+        DB::table('oefeningen')->insert([
+            'oefeningen' => 'Burpee',
+            'Beschrijving' => 'burpee.txt',
+            'foto' => 'burpee.jpg',
 
         ]);
     }
