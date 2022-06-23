aafaf0daaa450c34455c6c344a949cd0e74d92fe
ps217531 <ps217531@edu.summacollege.nl>
2022-06-23 13:17:58 +0200

    exercise

--- Nick/summaFit/App.js
+++ Nick/summaFit/App.js
@@ -1,59 +1,43 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, SafeAreaView, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button, TouchableOpacity, ImageBackground } from 'react-native';
 // import { TextInput } from 'react-native-paper';
 
 
 export default function App() {
 
-const [text, onChangeText] = React.useState("Useless Text");
-const [number, onChangeNumber] = React.useState(null);
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
   return (
-    <SafeAreaView style={styles.container}>
-         <TextInput
-        style={styles.input}
-        // onChangeText={onChangeNumber}
-        // value={number}
-        placeholder="Username"
-        // keyboardType="numeric"
-      /> 
-      <TextInput
-        style={styles.input}
-        // onChangeText={onChangeNumber}
-        // value={number}
-        placeholder="Password"
-        // keyboardType="numeric"
-      /> 
-      <Button
-      style={styles.bLogin}
-title="Inloggen"
-color="#59CBE8"
-/>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <View style={styles.container}>
+       <ImageBackground source={require('./assets/background_exercise.png')} resizeMode="contain" style={styles.image} >
+     <Text style={styles.textsem}  >oefeningen details</Text>
+  
+     <StatusBar style="auto" />
+
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#0FEED9',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    flexDirection:'row',  
+    alignItems:'center',
+    justifyContent:'center',
+    textAlignVertical: 'top',
+    paddingTop: 0,
+paddingBottom: 0
   },
-  input: {
-    height: 40,
-    margin: 12,
-   
-    borderBottomWidth:1,
-    padding: 10,
-    width: '35%',
-    textAlign : "center",
+
+  image: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  Blogin: {
-    height: 40,
-    margin: 12,
-    backgroundColor: '#59CBE8',
-   
+  textsem: {
+    height: "70%",
   },
 });
